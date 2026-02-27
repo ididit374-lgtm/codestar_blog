4de086f540bb40153b7b95a1f6b714fa072d230a
ididit374-lgtm <Ididit374@gmail.com>
2026-02-27 04:36:09 +0000

    readies code for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-q!-_e*d)wuh08!yl-0c(snx2k%2155)5@ve8ywx-43dfu$)*#9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [,'.herokuapp.com']
 
 
 # Application definition
