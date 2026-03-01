f3e10d4f023de5debdcd8ccee87802c0a8668a45
ididit374-lgtm <Ididit374@gmail.com>
2026-03-01 16:39:04 +0000

    Secret Key modification and Env file creation for database security.

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q!-_e*d)wuh08!yl-0c(snx2k%2155)5@ve8ywx-43dfu$)*#9'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
